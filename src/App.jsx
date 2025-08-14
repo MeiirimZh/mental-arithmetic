ee62dfba632b77c671e972036e7d5406d7890292
MeiirimZh <zh.meyirim@gmail.com>
2025-08-14 16:42:08 +0500

    added routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import TrainingPage from './pages/TrainingPage/TrainingPage'
+import StatisticsPage from './pages/StatisticsPage/StatisticsPage'
+import ResultPage from './pages/ResultPage/ResultPage'
 import './App.css'
 
 function App() {
   return (
     <>
       <Routes>
-
+        <Route path="/" element={<HomePage />} />
+        <Route path="/training" element={<TrainingPage />} />
+        <Route path="/statistics" element={<StatisticsPage />} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </>
   )
