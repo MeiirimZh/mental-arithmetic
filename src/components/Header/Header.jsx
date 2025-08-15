7c95f99de584d15b451d592ffd392f007448d4e1
MeiirimZh <zh.meyirim@gmail.com>
2025-08-15 20:02:05 +0500

    added nav on header, imported new font: WDXL Lubrifont TC

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <header>
-            
+        <header className="header wrapper">
+            <Link to="/" className="link">Mental Arithmetics</Link>
+            <nav>
+                <ul>
+                    <li><Link to="/training" className="nav-link link">Тренировка</Link></li>
+                    <li><Link to="/statistics" className="nav-link link">Статистика</Link></li>
+                </ul>
+            </nav>
         </header>
     )
 }
