dbd87325806f204cf2a3000a27fd3c1ac44ed544
MeiirimZh <zh.meyirim@gmail.com>
2025-08-15 20:04:35 +0500

    added Header to other pages

--- src/pages/ResultPage/ResultPage.jsx
+++ src/pages/ResultPage/ResultPage.jsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header/Header"
+
 export default function ResultPage() {
     return (
         <>
+            <Header></Header>
         </>
     )
 }
--- src/pages/StatisticsPage/StatisticsPage.jsx
+++ src/pages/StatisticsPage/StatisticsPage.jsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header/Header"
+
 export default function StatisticsPage() {
     return (
         <>
+            <Header></Header>
         </>
     )
 }
