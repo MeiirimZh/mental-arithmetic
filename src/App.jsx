07c9bae4a5d3e1df4f3e4120908c214ed46b2fec
MeiirimZh <zh.meyirim@gmail.com>
2025-08-18 17:38:46 +0500

    added generation of terms count

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import { useState } from 'react'
+
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import TrainingPage from './pages/TrainingPage/TrainingPage'
 import StatisticsPage from './pages/StatisticsPage/StatisticsPage'
 import ResultPage from './pages/ResultPage/ResultPage'
+
 import './App.css'
 
 function App() {
+  const [minNum, setMinNum] = useState(2)
+  const [maxNum, setMaxNum] = useState(2)
+
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/training" element={<TrainingPage />} />
+        <Route path="/training" element={
+          <TrainingPage minNum={ minNum } maxNum={ maxNum } />
+          } />
         <Route path="/statistics" element={<StatisticsPage />} />
         <Route path="/result" element={<ResultPage />} />
       </Routes>
--- src/pages/TrainingPage/TrainingPage.jsx
+++ src/pages/TrainingPage/TrainingPage.jsx
@@ -1,6 +1,12 @@
 import Header from "../../components/Header/Header"
 
-export default function TrainingPage() {
+export default function TrainingPage({ minNum, maxNum }) {
+    function generateTermsCount() {
+        return Math.floor(
+            Math.random() * (maxNum - minNum + 1)
+        ) + minNum
+    }
+    
     return (
         <>
             <Header></Header>
