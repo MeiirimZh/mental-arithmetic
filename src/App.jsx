917c977ddf235a3c2841516e3b0cbe5d43f4e2d6
MeiirimZh <zh.meyirim@gmail.com>
2025-08-18 18:07:50 +0500

    added generation of operations array

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,18 @@ function App() {
   const [minNum, setMinNum] = useState(2)
   const [maxNum, setMaxNum] = useState(2)
 
+  function pickRandom(array) {
+    return array[
+      Math.floor(Math.random() * array.length)
+    ]
+  }
+
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/training" element={
-          <TrainingPage minNum={ minNum } maxNum={ maxNum } />
+          <TrainingPage minNum={ minNum } maxNum={ maxNum } pickRandom={ pickRandom } />
           } />
         <Route path="/statistics" element={<StatisticsPage />} />
         <Route path="/result" element={<ResultPage />} />
--- src/pages/TrainingPage/TrainingPage.jsx
+++ src/pages/TrainingPage/TrainingPage.jsx
@@ -1,10 +1,22 @@
 import Header from "../../components/Header/Header"
 
-export default function TrainingPage({ minNum, maxNum }) {
+export default function TrainingPage({ minNum, maxNum, pickRandom }) {
+    const operations = ["+", "-", "*", "/"]
+
     function generateTermsCount() {
         return Math.floor(
             Math.random() * (maxNum - minNum + 1)
         ) + minNum
+    }
+
+    function generateOperations(count) {
+        let array = []
+
+        for (let i = 0; i < count; i++) {
+            array.push(pickRandom(operations))
+        }
+
+        return array
     }
     
     return (
