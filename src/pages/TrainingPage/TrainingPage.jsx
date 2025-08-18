1655ddcd21fea7a0a713c3e20bf3152b997e0cda
MeiirimZh <zh.meyirim@gmail.com>
2025-08-18 18:15:43 +0500

    added number selection percentages

--- src/pages/TrainingPage/TrainingPage.jsx
+++ src/pages/TrainingPage/TrainingPage.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react"
 import Header from "../../components/Header/Header"
 
 export default function TrainingPage({ minNum, maxNum, pickRandom }) {
+    // The percentage at which larger numbers are selected when adding and substracting
+    const [ASPercent, setASPercent] = useState(0.8)
+    // The percentage at which smaller numbers are selected when multiplying and dividing
+    const [MDPercent, setMDPercent] = useState(0.8)
     const operations = ["+", "-", "*", "/"]
 
     function generateTermsCount() {
