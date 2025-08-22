aed7f046b2776b2cce18798a7c211b3112bde26f
MeiirimZh <zh.meyirim@gmail.com>
2025-08-22 09:18:15 +0500

    refactored Background padding

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,31 +1,11 @@
 import './Background.css'
 
 export default function Background({ children, bgColor, padding }) {
-    let paddingLeft;
-    let paddingTop;
-    let paddingRight;
-    let paddingBottom;
-
-    if (padding.length === 2) {
-        paddingLeft = `${padding[0]}px`
-        paddingRight = `${padding[0]}px`
-
-        paddingTop = `${padding[1]}px`
-        paddingBottom = `${padding[1]}px`
-    }
-    else if (padding.length === 4) {
-        paddingLeft = `${padding[0]}px`
-        paddingTop = `${padding[1]}px`
-        paddingRight = `${padding[2]}px`
-        paddingBottom = `${padding[3]}px`
-    }
+    let paddingStyle = padding.map(x => `${x}px`).join(" ")
 
     const style = {
         "--bgColor": bgColor,
-        "--paddingLeft": paddingLeft,
-        "--paddingTop": paddingTop,
-        "--paddingRight": paddingRight,
-        "--paddingBottom": paddingBottom
+        "padding": paddingStyle
     }
 
     return (
